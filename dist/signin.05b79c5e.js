7b1f57a31ee8863a2a4594d526fc1853a5a46518
Ecool88 <kulik0v.e.e@yandex.ru>
2021-03-23 09:27:31 +0300

    add some change in package json 5

---
+++ dist/signin.05b79c5e.js
@@ -0,0 +1,18 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"P8me":[function(require,module,exports) {
+
+},{}],"yIHR":[function(require,module,exports) {
+"use strict";require("./main.scss");
+},{"./main.scss":"P8me"}],"IpfO":[function(require,module,exports) {
+"use strict";require("./headline.scss");
+},{"./headline.scss":"P8me"}],"ghSO":[function(require,module,exports) {
+"use strict";require("./signin-form.scss");
+},{"./signin-form.scss":"P8me"}],"pITW":[function(require,module,exports) {
+"use strict";require("./label-form.scss");
+},{"./label-form.scss":"P8me"}],"sfgC":[function(require,module,exports) {
+"use strict";function e(){console.log(document.getElementsByTagName("input"))}require("./button.scss"),console.log("button loaded");
+},{"./button.scss":"P8me"}],"YdGW":[function(require,module,exports) {
+"use strict";require("./button-link.scss");
+},{"./button-link.scss":"P8me"}],"mrBO":[function(require,module,exports) {
+"use strict";require("./signin.scss"),require("./../../main.js"),require("../../components/headline/headline.js"),require("../../blocks/signin-form/signin-form.js"),require("../../components/label-form/label-form.js"),require("../../components/button/button.js"),require("../../components/button-link/button-link.js");
+},{"./signin.scss":"P8me","./../../main.js":"yIHR","../../components/headline/headline.js":"IpfO","../../blocks/signin-form/signin-form.js":"ghSO","../../components/label-form/label-form.js":"pITW","../../components/button/button.js":"sfgC","../../components/button-link/button-link.js":"YdGW"}]},{},["mrBO"], null)
+//# sourceMappingURL=/signin.05b79c5e.js.map
